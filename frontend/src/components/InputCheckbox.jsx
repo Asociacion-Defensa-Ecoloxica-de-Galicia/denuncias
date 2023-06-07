98a027ba9b1564ec4601f1f0d3c5c79f459acf8c
Gabriel D <gabrieldavidfs@hotmail.com>
2023-06-07 19:18:24 +0200

    Added InputCheckbox Component

---
+++ frontend/src/components/InputCheckbox.jsx
@@ -0,0 +1,21 @@
+function InputCheckbox({labelText, name, value, legislation}){
+
+    function handlerCheckLegislation(event){
+        if (event.target.checked === true) {
+            legislation.add(event.target.value)
+        } else if (event.target.checked === false){
+            legislation.delete(event.target.value)
+        }
+        console.log(legislation);
+        
+    };
+
+    return(
+        <label>
+            {labelText}       
+            <input type="checkbox" onClick={handlerCheckLegislation} name={name} value={value} defaultChecked="false"/>
+        </label>
+    );
+};
+
+export default InputCheckbox;
