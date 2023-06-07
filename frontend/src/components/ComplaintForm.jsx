053a661b595efdfe6c7204de49cb9fd11292f0b3
Gabriel D <gabrieldavidfs@hotmail.com>
2023-06-07 19:50:26 +0200

    Added Style file to ComplaintForm

--- frontend/src/components/ComplaintForm.jsx
+++ frontend/src/components/ComplaintForm.jsx
@@ -1,5 +1,7 @@
 import InputCheckbox from "./InputCheckbox";
 
+import styles from "./ComplaintForm.module.css"
+
 function ComplaintForm() {
 
     const legislation = new Set()
@@ -8,53 +10,95 @@ function ComplaintForm() {
     return(
         <>
             <form>
-                <h2>Codigo de ADEGA:</h2>
-                    <label>
+                <label>Codigo de ADEGA:
                         DF<input type="text"/>
+                </label>
+
+                <label>Asunto Relacionado
+                    <textarea cols="60" rows="10"></textarea>
+                </label>
+
+                <label>Recentemente, membros de ADEGA detectaron:
+                    <textarea 
+                        cols="60" 
+                        rows="10" 
+                        placeholder="Incluír a data aproximada, unha descrición 
+                                    dos feitos denunciados, e indicar a referencia 
+                                    catastral e/ou cordeadas X/Y se as houbese"
+                    />
+                </label>
+                    
+                <fieldset>
+                    <legend>Datos da ubicación</legend>
+                    <label>
+                        Polígono: 
+                        <input type="text"/>
                     </label>
-                <h2>Asunto Relacionado</h2>
-                <textarea cols="30" rows="10"></textarea>
-                <h2>Expón (máx. 1995 caracteres, senón a denuncia ten que ir apegada)</h2>
-                <div>
-                    <div>
-                        <div>Recentemente, membros de ADEGA detectaron</div>
-                        <textarea 
-                            cols="30" 
-                            rows="10" 
-                            placeholder="Incluír a data aproximada, unha descrición 
-                                        dos feitos denunciados, e indicar a referencia 
-                                        catastral e/ou cordeadas X/Y se as houbese"
-                        />
-                        <span>, no </span>
-                        <label>
-                            polígono 
-                            <input type="text"/>
-                        </label>
-                        <label>
-                            Parcela 
-                            <input type="text"/>
-                        </label>
-                        <label>
-                            no Concello de 
-                            <input type="text"/>
-                        </label>
-                        <label>
-                            Provincia de 
-                            <input type="text"/>
-                        </label>
-                        <p>
-                            incumprindo o establecido na Lei 7/2012, do 28 de xuño, de montes de Galicia, concretamente: 
-                        </p>
-                    </div>
-                    <fieldset>
-                        <InputCheckbox
-                            labelText="Artigo 60 regula os cambios de actividade forestal a agrícola."
-                            name="legislacion"
-                            value="Art60"
-                            legislation={legislation}
-                        />
-                    </fieldset>
-                </div>
+                    <label>
+                        Parcela: 
+                        <input type="text"/>
+                    </label>
+                    <label>
+                        Concello:
+                        <input type="text"/>
+                    </label>
+                    <label>
+                        Provincia:
+                        <input type="text"/>
+                    </label>
+                </fieldset>
+
+                <fieldset>
+                    <InputCheckbox
+                        labelText="Artigo 60 regula os cambios de actividade forestal a agrícola."
+                        name="legislacion"
+                        value="Art60"
+                        legislation={legislation}
+                    />
+                    <InputCheckbox
+                        labelText="Artigo 61 regula os cambios de actividade agrícola a forestal."
+                        name="legislacion"
+                        value="Art61"
+                        legislation={legislation}
+                    />
+                    <InputCheckbox
+                        labelText="Artigo 67.4 establece a prohibición das reforestacións e novas plantacións con eucaliptos nas superficies poboadas por frondosas autóctonas. "
+                        name="legislacion"
+                        value="Art67_4"
+                        legislation={legislation}
+                    />
+                    <InputCheckbox
+                        labelText="Artigo 67.5 establece a necesidade dunha autorización para novas plantacións de eucalipto superiores a 5ha."
+                        name="legislacion"
+                        value="Art67_5"
+                        legislation={legislation}
+                    />
+                    <InputCheckbox
+                        labelText="Artigo 68 relativo ás distancias das repoboacións."
+                        name="legislacion"
+                        value="Art68"
+                        legislation={legislation}
+                    />
+                    <InputCheckbox
+                        labelText="Artigo 92 establece as condicións para a realización de cortas e outros aproveitamentos madeireiros."
+                        name="legislacion"
+                        value="Art62"
+                        legislation={legislation}
+                    />
+                    <InputCheckbox
+                        labelText="Artigo 93 relacionado co aproveitamento en masas consolidadas de frondosas autóctonas"
+                        name="legislacion"
+                        value="Art93"
+                        legislation={legislation}
+                    />
+                    <InputCheckbox
+                        labelText="Artigo 104 establece o comercio responsable de produtos forestais e a loita contra a madeira ilegal."
+                        name="legislacion"
+                        value="Art104"
+                        legislation={legislation}
+                    />
+                </fieldset>
+
             </form>
         </>
 
