fcaf498e0395d67002232eed7b277ec4623657da
Gabriel D <gabrieldavidfs@hotmail.com>
2023-06-14 19:17:45 +0200

    Divided the legislation array into 2 for the 2 section on the document

--- frontend/src/components/ComplaintForm.jsx
+++ frontend/src/components/ComplaintForm.jsx
@@ -1,12 +1,14 @@
-import InputCheckbox from "./InputCheckbox";
-
-import styles from "./ComplaintForm.module.css"
 import { useEffect, useState } from "react";
+import InputCheckbox from "./InputCheckbox";
+import PdfBuilder from "./PdfBuilder";
+import styles from "./ComplaintForm.module.css"
+
 
 function ComplaintForm() {
 
 
-    const [ legislation, setLegislation ] = useState(new Set())
+    const [ legislationSection1, setLegislationSection1 ] = useState(new Set())
+    const [ legislationSection2, setLegislationSection2 ] = useState(new Set())
     const [ isTerritorial , setIsTerritorial ] = useState(false)
     const formDataState = useState({})
     const [ formData, setFormData ] = formDataState
@@ -14,10 +16,12 @@ function ComplaintForm() {
 
     useEffect(
         ()=>{
-            formData.legislation = Array.from(legislation)
-            setIsOtherChecked(formData?.legislation?.includes("Outros"))
+            formData.legislationSection1 = Array.from(legislationSection1)
+            formData.legislationSection2 = Array.from(legislationSection2)
+            setIsOtherChecked(formData?.legislationSection2?.includes("Outros"))
+            console.log("S1: ", legislationSection1, "S2: ", legislationSection2);
         },
-        [legislation]
+        [legislationSection1, legislationSection2]
     );
 
     function saveToStatePropertyFactory(objectState){
@@ -111,77 +115,77 @@ function ComplaintForm() {
                     <legend>Marca leis incumpridas</legend>
                     <InputCheckbox
                         labelText="Artigo 60 regula os cambios de actividade forestal a agrícola."
-                        name="legislation" value="art60" legislation={legislation} setLegislation={setLegislation}
+                        name="legislation" value="art60" legislation={legislationSection1} setLegislation={setLegislationSection1}
                     />
                     <InputCheckbox
                         labelText="Artigo 61 regula os cambios de actividade agrícola a forestal."
-                        name="legislation" value="art61" legislation={legislation} setLegislation={setLegislation}
+                        name="legislation" value="art61" legislation={legislationSection1} setLegislation={setLegislationSection1}
                     />
                     <InputCheckbox
                         labelText="Artigo 67.4 establece a prohibición das reforestacións e novas plantacións con eucaliptos nas superficies poboadas por frondosas autóctonas. "
-                        name="legislation" value="art67_4" legislation={legislation} setLegislation={setLegislation}
+                        name="legislation" value="art67_4" legislation={legislationSection1} setLegislation={setLegislationSection1}
                     />
                     <InputCheckbox
                         labelText="Artigo 67.5 establece a necesidade dunha autorización para novas plantacións de eucalipto superiores a 5ha."
-                        name="legislation" value="art67_5" legislation={legislation} setLegislation={setLegislation}
+                        name="legislation" value="art67_5" legislation={legislationSection1} setLegislation={setLegislationSection1}
                     />
                     <InputCheckbox
                         labelText="Artigo 68 relativo ás distancias das repoboacións."
-                        name="legislation" value="art68" legislation={legislation} setLegislation={setLegislation}
+                        name="legislation" value="art68" legislation={legislationSection1} setLegislation={setLegislationSection1}
                     />
                     <InputCheckbox
                         labelText="Artigo 92 establece as condicións para a realización de cortas e outros aproveitamentos madeireiros."
-                        name="legislation" value="art62" legislation={legislation} setLegislation={setLegislation}
+                        name="legislation" value="art62" legislation={legislationSection1} setLegislation={setLegislationSection1}
                     />
                     <InputCheckbox
                         labelText="Artigo 93 relacionado co aproveitamento en masas consolidadas de frondosas autóctonas"
-                        name="legislation" value="art93" legislation={legislation} setLegislation={setLegislation}
+                        name="legislation" value="art93" legislation={legislationSection1} setLegislation={setLegislationSection1}
                     />
                     <InputCheckbox
                         labelText="Artigo 104 establece o comercio responsable de produtos forestais e a loita contra a madeira ilegal."
-                        name="legislation" value="art104" legislation={legislation} setLegislation={setLegislation}
+                        name="legislation" value="art104" legislation={legislationSection1} setLegislation={setLegislationSection1}
                     />
                 </fieldset>
                 <fieldset>
                     <InputCheckbox
                         labelText="Lei 11/2021, do 14 de maio, de recuperación da terra agraria de Galicia"
-                        name="legislation" value="lei11_2021" legislation={legislation} setLegislation={setLegislation}
+                        name="legislation" value="lei11_2021" legislation={legislationSection2} setLegislation={setLegislationSection2}
                     />
                     <InputCheckbox
                         labelText="Lei 3/2007 de 9 de abril, de prevención e defensa contra os incendios forestais de Galicia. "
-                        name="legislation" value="Lei3_2007" legislation={legislation} setLegislation={setLegislation}
+                        name="legislation" value="Lei3_2007" legislation={legislationSection2} setLegislation={setLegislationSection2}
                     />
                     <InputCheckbox
                         labelText="Lei 5/2016 do 4 de maio do patrimonio cultural de Galicia."
-                        name="legislation" value="lei5_2016" legislation={legislation} setLegislation={setLegislation}
+                        name="legislation" value="lei5_2016" legislation={legislationSection2} setLegislation={setLegislationSection2}
                     />
                     <InputCheckbox
                         labelText="Lei 5/2019, do 2 de agosto, do patrimonio natural e da Biodiversidade de Galicia."
-                        name="legislation" value="lei5_2019" legislation={legislation} setLegislation={setLegislation}
+                        name="legislation" value="lei5_2019" legislation={legislationSection2} setLegislation={setLegislationSection2}
                     />
                     <InputCheckbox
                         labelText="Real Decreto 849/1986, de 11 de abril, por el que se aprueba el Reglamento del Dominio Público Hidráulico, que desarrolla los títulos preliminar I, IV, V, VI y VII de la Ley 29/1985, de 2 de agosto, de Aguas."
-                        name="legislation" value="rdec849_1986" legislation={legislation} setLegislation={setLegislation}
+                        name="legislation" value="rdec849_1986" legislation={legislationSection1} setLegislation={setLegislationSection1}
                     />
                     <InputCheckbox
                         labelText="Real Decreto 35/2023, de 24 de enero, por el que se aprueba la revisión de los planes hidrológicos de las demarcaciones hidrográficas del Cantábrico Occidental, Guadalquivir, Ceuta, Melilla, Segura y Júcar, y de la parte española de las demarcaciones hidrográficas del Cantábrico Oriental, Miño-Sil, Duero, Tajo, Guadiana y Ebro."
-                        name="legislation" value="rdec35_2023" legislation={legislation} setLegislation={setLegislation}
+                        name="legislation" value="rdec35_2023" legislation={legislationSection2} setLegislation={setLegislationSection2}
                     />
                     <InputCheckbox
                         labelText="Decreto 37/2014, do 27 de marzo, polo que se declaran zonas especiais de conservación os lugares de importancia comunitaria de Galicia e se aproba o Plan director da Rede Natura 2000 de Galicia."
-                        name="legislation" value="dec37_2014" legislation={legislation} setLegislation={setLegislation}
+                        name="legislation" value="dec37_2014" legislation={legislationSection2} setLegislation={setLegislationSection2}
                     />
                     <InputCheckbox
                         labelText="Lei 33/2015, do 21 de setembro, pola que se modifica a Lei 42/2007, do 13 de decembro, do patrimonio natural e da biodiversidade."
-                        name="legislation" value="lei33_2015" legislation={legislation} setLegislation={setLegislation}
+                        name="legislation" value="lei33_2015" legislation={legislationSection2} setLegislation={setLegislationSection2}
                     />
                     <InputCheckbox
                         labelText="Lei 45/2007 Desarrollo sostenible del medio rural"
-                        name="legislation" value="lei45_2007" legislation={legislation} setLegislation={setLegislation}
+                        name="legislation" value="lei45_2007" legislation={legislationSection2} setLegislation={setLegislationSection2}
                     />
                     <InputCheckbox
                         labelText="Outros"
-                        name="legislation" value="outros" legislation={legislation} setLegislation={setLegislation}
+                        name="legislation" value="outros" legislation={legislationSection2} setLegislation={setLegislationSection2}
                     />
                     {isOtherChecked && 
                         <textarea
@@ -252,9 +256,7 @@ function ComplaintForm() {
                 
 
             </form>
-            <template>
-                <p>Non estou</p>
-            </template>
+            <PdfBuilder formDataState={formDataState}/>
         </>
 
         
