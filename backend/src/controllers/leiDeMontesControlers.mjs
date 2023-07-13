b38f2748c5b11b9f00f397bbadb769ad8a1ef406
man80vil <123630768+man80vil@users.noreply.github.com>
2023-07-13 13:30:53 +0200

    Engade un endpoint para obter os artigos da lei de montes

---
+++ backend/src/controllers/leiDeMontesControlers.mjs
@@ -0,0 +1,14 @@
+/** @typedef {import("express").RequestHandler} RequestHandler */
+
+import { LeyDeMontes } from "../db.mjs";
+
+/** @type {RequestHandler} */
+async function todosOsArtigosLeiDeMontes(peticion, resposta) {
+    try {
+        const artigosLeiDeMontes  = await LeyDeMontes.findAll()
+        resposta.json(artigosLeiDeMontes)
+    } catch (error) {
+        console.error(error)
+        resposta.sendStatus(500)
+    }
+}
--- backend/src/express.mjs
+++ backend/src/express.mjs
@@ -1,13 +1,18 @@
 import express from "express"
 import { corsMiddleware } from "./middlewares/cors.mjs"
 import { rootController } from "./controllers/rootController.mjs"
+import { todosOsArtigosLeiDeMontes } from "./controllers/leiDeMontesControlers.mjs"
 
 const BASE_PATH = "/api/v0.0/"
 
 const app = express()
 app.use(corsMiddleware)
 
+/** Endpoint só para tests */
 app.get(BASE_PATH, rootController)
+
+/** Endpoint para obter lista de artigos lei de montes */
+app.get(BASE_PATH+"artigos-lei-de-montes", todosOsArtigosLeiDeMontes)
 
 export {
     app
