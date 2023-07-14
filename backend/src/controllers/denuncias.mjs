263cac21c49b5ea4c2ad74eff1b31c590b86192c
0IvanTC <ivantc@outlook.com>
2023-07-14 17:10:27 +0200

    Funcion para crear novas denuncias

--- backend/src/controllers/denuncias.mjs
+++ backend/src/controllers/denuncias.mjs
@@ -1,8 +1,37 @@
 /** @typedef {import("express").RequestHandler} RequestHandler */
 
 /** @type {RequestHandler} */
-function novaDenuncia(peticion, resposta) {
+
+import { Denuncia } from "../db.mjs"
+
+ async function novaDenuncia(peticion, resposta) {
     // TODO: Crea unha nova denuncia co obxeto en peticion.body
+
+    try {
+    
+        const {provinceOfComplaint,adegaCode,issue,complaintDetails,zone,allotment,municipality,otherLegislation,institution,institutionProvince,isOtherLegislationChecked,extraDocumentation} = peticion.body
+    
+        const nuevaDenuncia = await Denuncia.create({
+            provinceOfComplaint,
+            adegaCode,
+            issue,
+            complaintDetails,
+            zone,
+            allotment,
+            municipality,
+            otherLegislation,
+            institution,
+            institutionProvince,
+            isOtherLegislationChecked,
+            extraDocumentation
+        });
+        resposta.json(nuevaDenuncia)
+    
+      } catch (error) {
+        console.error(error);
+        console.status(500);
+        console.send("Error al introducir los datos")
+      }
 }
 
 export {
