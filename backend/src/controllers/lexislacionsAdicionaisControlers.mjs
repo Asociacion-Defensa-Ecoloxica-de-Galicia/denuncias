4d98ac2c1f87e20c20ac27fe813d1b7db3c41b9f
0IvanTC <ivantc@outlook.com>
2023-07-14 17:09:43 +0200

    Creadas las funciones para obtener las normativas

--- backend/src/controllers/lexislacionsAdicionaisControlers.mjs
+++ backend/src/controllers/lexislacionsAdicionaisControlers.mjs
@@ -1,8 +1,16 @@
 /** @typedef {import("express").RequestHandler} RequestHandler */
 
 /** @type {RequestHandler} */
-function todasAsLexislacionsAdicionais(peticion, resposta) {
+async function todasAsLexislacionsAdicionais(peticion, resposta) {
     // TODO: Obter lista de todalas lexislacions adicionais e entrgala como JSON
+    try {
+        const artigosOutrasNormativas = await OutrasNormativas.findAll();
+        resposta.json(artigosOutrasNormativas);
+    } catch (error) {
+        console.error(error);
+        console.status(500);
+        console.send("Error al obtener las normativas");
+    }
 }
 
 export {
